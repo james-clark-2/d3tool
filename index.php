3547f89148347646c77857890d74dca623085722
mentalmorph <jclark1279@gmail.com>
2014-11-13 17:45:12 -0700

    Profile factory JSON test

--- index.php
+++ index.php
@@ -1,22 +1,10 @@
 <?php
+require_once('library/D3ProfileFactory.php');
 
-$btag_name = 'atenhara';
-$btag_code = '1797';
+$factory = new D3ProfileFactory();
 
-$profile_url = 'us.battle.net/api/d3/profile/'.$btag_name.'-'.$btag_code.'/';
+$profile = $factory->getProfile('atenhara-1797');
 
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $profile_url,
-    CURLOPT_USERAGENT => 'd3tool test'
-));
-
-$result = curl_exec($curl);
-
-curl_close($curl);
-
-echo $result;
+echo $profile;
 
 ?>
